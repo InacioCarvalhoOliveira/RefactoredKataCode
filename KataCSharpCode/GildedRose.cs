e9186d294d98c2050e30f6e0ff70af6d286ef814
Inacio Carvalho <inacio.carvalho.oliveria02@gmail.com>
2022-11-07 17:11:28 -0300

    some adjusts neet to be applied

---
+++ KataCSharpCode/GildedRose.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose : QualityAnalysis
+    {
+         IList<Item> Items;
+         public GildedRose(IList<Item> Items)
+         {
+             this.Items = Items;
+         }
+
+        public void UpdateQuality()
+        {
+            foreach (Item v in Items)
+            {
+                calcMeasureQuality(v);
+
+                if (v.Name != sulfurasContext())
+                {
+                    v.SellIn = v.SellIn - 1;
+                }
+
+                if (v.SellIn < 0)
+                {                   
+                    if (v.Name != agedBrieContext() && v.Name != backstageContext() )
+                    {                        
+                        if (v.Quality > 0)
+                        {
+                            calcQualityPoints(v);
+                        }
+                        
+                        else
+                        {
+                            v.Quality = v.Quality - v.Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (v.Quality < 50)
+                        {
+                            v.Quality = v.Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+        // private void AvaliarQualidade(Item v)
+        // {
+        //      if (v.Name != agedBrieContext() && v.Name != backstageContext())
+        //      {
+        //          PontuarQualidade(v);
+        //      }
+        //      else
+        //      {
+        //          if (v.Quality < 50)
+        //          {
+        //              v.Quality = v.Quality + 1;
+        //             if (v.Name == backstageContext())
+        //              {
+        //                  PontuarVenda(v);
+        //              }
+        //          }
+        //      }
+        // }
+
+        // private static void PontuarVenda(Item v)
+        // {
+        //     if (v.SellIn < 11 || v.SellIn < 6)
+        //     {
+        //         if (v.Quality < 50)
+        //         {
+        //             v.Quality = v.Quality + 1;
+        //         }
+        //     }
+        // }
+
+        // private void PontuarQualidade(Item v)
+        // {
+        //     if (v.Quality > 0)
+        //     {
+        //         if (v.Name != sulfurasContext())
+        //         {
+        //             v.Quality = v.Quality - 1;
+        //         }
+        //     }
+        // }
+
+    }
+}
---
+++ KataCSharpCode/contextQuality/ContextComments.cs
@@ -0,0 +1,35 @@
+namespace csharp
+{
+    public class ContextComments : Item
+    {
+        private string AgedBrie { get; set; }
+        private string Backstage { get; set; }
+        private string Sulfuras { get; set; }
+       
+        public ContextComments(){}
+      
+        public ContextComments(string agedBrie, string backstage, string sulfuras)
+        {
+            this.AgedBrie  = agedBrie;
+            this.Backstage = backstage;
+            this.Sulfuras = sulfuras;   
+        }
+
+        public string agedBrieContext()
+        {
+            AgedBrie = "Aged Brie";
+            return AgedBrie;
+        }
+        public string backstageContext()
+        {
+            Backstage = "Backstage passes to a TAFKAL80ETC concert";
+            return Backstage;
+        }
+        public string sulfurasContext()
+        {
+            Sulfuras = "ASulfuras, Hand of Ragnaros";
+            return Sulfuras;
+        }
+
+    }
+}
